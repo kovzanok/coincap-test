44c89fcc9260cc23718bec45469892df89ab2bb0
Alex Kovzan <alexanderkovzan@gmail.com>
2023-08-04 18:20:24 +0300

    feat: add types

---
+++ src/types.ts
@@ -0,0 +1,18 @@
+type CryptoListType = Pick<
+  CryptoType,
+  "name" | "symbol" | "changePercent24Hr" | "id"
+>;
+
+type CryptoType = {
+  id: string;
+  rank: string;
+  symbol: string;
+  name: string;
+  supply: string;
+  maxSupply: string | null;
+  marketCapUsd: string;
+  volumeUsd24Hr: string;
+  priceUsd: string;
+  changePercent24Hr: string | null;
+  vwap24Hr: string;
+};
