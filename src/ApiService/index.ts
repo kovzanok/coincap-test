159569e2c0b31a42cda4a6644571bde7fc9b2d8f
Alex Kovzan <alexanderkovzan@gmail.com>
2023-08-05 14:31:28 +0300

    feat: add api service

---
+++ src/ApiService/index.ts
@@ -0,0 +1,16 @@
+export class ApiService {
+  static BASE_URL = "https://api.coincap.io";
+  static LIMIT = "20";
+
+  static async getAllCrypto(page: number) {
+    const url = new URL("/v2/assets", ApiService.BASE_URL);
+    const searchParams = new URLSearchParams([
+      ["limit", ApiService.LIMIT],
+      ["offset", `${+ApiService.LIMIT * page}`],
+    ]);
+    const res = await fetch(`${url}?${searchParams.toString()}`);
+    const { data }: { data: CryptoType[]; timestamp: number } =
+      await res.json();
+    return data;
+  }
+}
