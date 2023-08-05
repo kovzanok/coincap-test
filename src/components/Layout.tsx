e6bceca5eb313757a120d321f36e2278b73634c8
Alex Kovzan <alexanderkovzan@gmail.com>
2023-08-05 23:01:57 +0300

    feat: save porfolio on page close

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,17 @@
+import { useEffect, useContext } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import { portfolioContext } from "../providers/PorfolioProvider";
 
 export default function Layout() {
+  const { portfolio } = useContext(portfolioContext);
+
+  useEffect(() => {
+    window.onunload = () => {
+      localStorage.setItem("portfolio", JSON.stringify(portfolio));
+    };
+  }, [portfolio]);
+
   return (
     <>
       <Header />
