69f420b9c5ee2e4084399153148bdc1e677f01c0
Alex Kovzan <alexanderkovzan@gmail.com>
2023-08-05 14:41:32 +0300

    feat: add page provider to app component

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import "./App.scss";
 import Layout from "./components/Layout";
 import Main from "./pages/Main";
 import Crypto from "./pages/Crypto";
+import  PageProvider  from "./providers/PageProvider";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<Main />} />
-          <Route path='/:id' element={<Crypto />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <PageProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Main />} />
+            <Route path='/:id' element={<Crypto />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </PageProvider>
   );
 }
 
