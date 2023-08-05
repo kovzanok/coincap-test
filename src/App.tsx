cc494893e230eb8b19cc0e45e10b74063b1bd742
Alex Kovzan <alexanderkovzan@gmail.com>
2023-08-05 15:29:41 +0300

    fix: remove deleted provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,17 @@ import "./App.scss";
 import Layout from "./components/Layout";
 import Main from "./pages/Main";
 import Crypto from "./pages/Crypto";
-import  PageProvider  from "./providers/PageProvider";
 
 function App() {
   return (
-    <PageProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Layout />}>
-            <Route index element={<Main />} />
-            <Route path='/:id' element={<Crypto />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </PageProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Main />} />
+          <Route path='/:id' element={<Crypto />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
