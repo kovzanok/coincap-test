43debf2c45b6a7340c7c5264b36bb209a886b238
Alex Kovzan <alexanderkovzan@gmail.com>
2023-08-05 23:01:05 +0300

    feat: change get all crypto method

--- src/ApiService/index.ts
+++ src/ApiService/index.ts
@@ -2,15 +2,17 @@ export class ApiService {
   static BASE_URL = "https://api.coincap.io";
   static LIMIT = 20;
 
-  static async getAllCrypto(
-    page: number,
-    signal: AbortSignal,
-    limit = ApiService.LIMIT
-  ): Promise<CryptoType[]> {
+  static async getAllCrypto({
+    page = 0,
+    signal,
+    ids = [],
+    limit = ApiService.LIMIT,
+  }: AllCryptoRequestParams): Promise<CryptoType[]> {
     const url = new URL("/v2/assets", ApiService.BASE_URL);
     const searchParams = new URLSearchParams([
       ["limit", String(limit)],
       ["offset", `${limit * page}`],
+      ["ids", ids.toString()],
     ]);
     const res = await fetch(`${url}?${searchParams.toString()}`, { signal });
     const { data }: { data: CryptoType[]; timestamp: number } =
