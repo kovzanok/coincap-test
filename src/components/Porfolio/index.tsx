a37eb060561008e138c1feb753bc605e870e816e
Alex Kovzan <alexanderkovzan@gmail.com>
2023-08-05 10:48:53 +0300

    feat: add styles to porfolio component

--- src/components/Porfolio/index.tsx
+++ src/components/Porfolio/index.tsx
@@ -1,13 +1,16 @@
 import cls from "./Porfolio.module.scss";
 
 export default function Portfolio() {
-  const prevSum = 134.32;
+  const initialSum = 134.32;
   const add = 2.38;
-  const diff = prevSum / (prevSum + add);
+  const diff = ((add) * 100) / initialSum;
 
   return (
     <div className={cls.portfolio}>
-      {prevSum} USD + <span className={cls['portfolio__add']}>{add}</span> ({diff.toFixed(2)}%)
+      <div className={cls.title}>Your portfolio</div>
+      <span className={cls.initial}>$ {initialSum}</span> +{" "}
+      <span className={cls["portfolio__add"]}>{add}</span>{" "}
+      <span className={cls.diff}>{diff.toFixed(2)}%</span>
     </div>
   );
 }
