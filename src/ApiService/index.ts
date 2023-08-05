cdba70f30be22aaeabdf37449d4b27c11dc96084
Alex Kovzan <alexanderkovzan@gmail.com>
2023-08-05 14:42:29 +0300

    refactor: add return type

--- src/ApiService/index.ts
+++ src/ApiService/index.ts
@@ -2,7 +2,7 @@ export class ApiService {
   static BASE_URL = "https://api.coincap.io";
   static LIMIT = "20";
 
-  static async getAllCrypto(page: number) {
+  static async getAllCrypto(page: number): Promise<CryptoType[]> {
     const url = new URL("/v2/assets", ApiService.BASE_URL);
     const searchParams = new URLSearchParams([
       ["limit", ApiService.LIMIT],
