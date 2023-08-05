127e09e6130e18d6d280ada10d2d88acd99ce1a9
Alex Kovzan <alexanderkovzan@gmail.com>
2023-08-05 10:50:26 +0300

    feat: add crypto change info for crypto list

--- src/components/PopularCryptoList/index.tsx
+++ src/components/PopularCryptoList/index.tsx
@@ -1,3 +1,4 @@
+import { calcBgChange, calcColorChange, stringToFixed } from "../../utils";
 import cls from "./PopularCryptoList.module.scss";
 
 export default function PopularCryptoList() {
@@ -7,32 +8,45 @@ export default function PopularCryptoList() {
       name: "Bitcoin",
       symbol: "BTC",
       priceUsd: "6929.8217756835584756",
+      changePercent24Hr: "-0.81",
     },
     {
       id: "ethereum",
       symbol: "ETH",
       name: "Ethereum",
       priceUsd: "404.9774667045200896",
+      changePercent24Hr: "1.2",
     },
     {
       id: "bitcoin",
       name: "XRP",
       symbol: "XRP",
       priceUsd: "0.4202870472643482",
+      changePercent24Hr: "-0.2",
     },
   ];
 
   return (
     <ul className={cls["list"]}>
-      {popularCrypto.map(({ name, symbol, priceUsd }, idx) => (
-        <li className={cls["list__item"]} key={idx}>
-          <div className={cls["list__name"]}>{name}</div>
-          <div className={cls["list__symbol"]}>{symbol}</div>
-          <div className={cls["list__change"]}>
-            {Number(priceUsd).toFixed(5)}$
-          </div>
-        </li>
-      ))}
+      {popularCrypto.map(
+        ({ name, symbol, priceUsd, changePercent24Hr }, idx) => (
+          <li className={cls["list__item"]} key={idx}>
+            <div>{name}</div>
+            <div className={cls["list__symbol"]}>{symbol}</div>
+            <div
+              style={{
+                backgroundColor: calcBgChange(changePercent24Hr),
+              }}
+              className={cls["list__money"]}
+            >
+              <div>${stringToFixed(priceUsd, 3)}</div>
+              <div style={{ color: calcColorChange(changePercent24Hr) }}>
+                {changePercent24Hr}%
+              </div>
+            </div>
+          </li>
+        )
+      )}
     </ul>
   );
 }
