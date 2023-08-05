083a36d3226d55de9dfbde1092324aed7d1ca82b
Alex Kovzan <alexanderkovzan@gmail.com>
2023-08-05 15:30:03 +0300

    feat: add limit to method args

--- src/ApiService/index.ts
+++ src/ApiService/index.ts
@@ -1,12 +1,15 @@
 export class ApiService {
   static BASE_URL = "https://api.coincap.io";
-  static LIMIT = "20";
+  static LIMIT = 20;
 
-  static async getAllCrypto(page: number): Promise<CryptoType[]> {
+  static async getAllCrypto(
+    page: number,
+    limit = ApiService.LIMIT
+  ): Promise<CryptoType[]> {
     const url = new URL("/v2/assets", ApiService.BASE_URL);
     const searchParams = new URLSearchParams([
-      ["limit", ApiService.LIMIT],
-      ["offset", `${+ApiService.LIMIT * page}`],
+      ["limit", String(limit)],
+      ["offset", `${limit * page}`],
     ]);
     const res = await fetch(`${url}?${searchParams.toString()}`);
     const { data }: { data: CryptoType[]; timestamp: number } =
