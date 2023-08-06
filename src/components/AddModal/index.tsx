1b17f7db168d860d51bac4ca586a87a97fbdbe9f
Alex Kovzan <alexanderkovzan@gmail.com>
2023-08-06 09:23:04 +0300

    feat: porfolio add feature

--- src/components/AddModal/index.tsx
+++ src/components/AddModal/index.tsx
@@ -1,12 +1,14 @@
+import { useContext, useState } from "react";
 import Button from "../Button";
 import cls from "./AddModal.module.scss";
+import { portfolioContext } from "../../providers/PorfolioProvider";
 
 type AddModalProps = {
   opened: boolean;
   name: string;
   symbol: string;
   id: string;
-  close: React.MouseEventHandler;
+  close: () => void;
 };
 
 export default function AddModal({
@@ -16,21 +18,58 @@ export default function AddModal({
   opened,
   close,
 }: AddModalProps) {
+  const [value, setValue] = useState("");
+  const { portfolio, setPortfolio, setLastCrypto } =
+    useContext(portfolioContext);
+
+  const closeModal = () => {
+    setValue("");
+    close();
+  };
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    const addedCryptoIndex = portfolio.findIndex((item) => item.id === id);
+    if (addedCryptoIndex !== -1) {
+      const addedCrypto = portfolio[addedCryptoIndex];
+      setPortfolio([
+        ...portfolio.slice(0, addedCryptoIndex),
+        { id, amount: Number(value) + addedCrypto.amount },
+        ...portfolio.slice(addedCryptoIndex + 1),
+      ]);
+    } else {
+      setPortfolio([...portfolio, { id, amount: Number(value) }]);
+    }
+    setLastCrypto({ id, amount: Number(value) });
+    closeModal();
+  };
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setValue(e.target.value);
+  };
+
   if (!opened) return null;
 
   return (
     <div className={cls.modal}>
       <div className={cls.body}>
         <h1 className={cls.title}>Add {name} to portfolio</h1>
-        <form className={cls.form}>
+        <form onSubmit={handleSubmit} className={cls.form}>
           <label className={cls.label}>
-            <input className={cls.input} min={0} type='number' />
+            <input
+              name='amount'
+              value={value}
+              onChange={handleChange}
+              className={cls.input}
+              min={0}
+              type='number'
+            />
             {symbol}
           </label>
           <Button>Add crypto</Button>
         </form>
         <div className={cls.button}>
-          <Button onClick={close}>Back</Button>
+          <Button onClick={closeModal}>Back</Button>
         </div>
       </div>
     </div>
