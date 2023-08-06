2307e5e1a8c7923e723c4f528348efbf07ea2c82
Alex Kovzan <alexanderkovzan@gmail.com>
2023-08-06 09:29:05 +0300

    feat: update porfolio change on every crypto added

--- src/components/Porfolio/index.tsx
+++ src/components/Porfolio/index.tsx
@@ -1,16 +1,66 @@
+import { useContext, useState } from "react";
 import cls from "./Porfolio.module.scss";
+import { portfolioContext } from "../../providers/PorfolioProvider";
+import { useFetching } from "../../hooks";
+import { ApiService } from "../../ApiService";
+import PortfolioModal from "../PortfolioModal";
+import Loader from "../Loader";
 
 export default function Portfolio() {
-  const initialSum = 134.32;
-  const add = 2.38;
-  const diff = ((add) * 100) / initialSum;
+  const [crypto, setCrypto] = useState<
+    ({ amount: number } & Pick<CryptoType, "priceUsd" | "id">)[]
+  >([]);
+  const [loading, setLoading] = useState(true);
+  const [open, setOpen] = useState(false);
+  const { portfolio, lastCrypto } = useContext(portfolioContext);
+  const ids = portfolio.map(({ id }) => id);
+  useFetching(
+    (signal) => ApiService.getAllCrypto({ signal, ids }),
+    (res: CryptoType[]) => {
+      setCrypto(
+        portfolio.length
+          ? res.map(({ priceUsd, id }, idx) => ({
+              priceUsd,
+              id,
+              amount: portfolio[idx].amount,
+            }))
+          : []
+      );
+      setLoading(false);
+    },
+    [ids.length, lastCrypto]
+  );
+  const sum = crypto.reduce(
+    (sum, { priceUsd, amount }) => (sum += amount * Number(priceUsd)),
+    0
+  );
 
+  const lastCryptoObj = crypto.find(({ id }) => id === lastCrypto.id);
+
+  const add = lastCryptoObj
+    ? lastCrypto.amount * Number(lastCryptoObj.priceUsd)
+    : 0;
+
+  const initialSum = sum - add;
+  const diff = (add * 100) / initialSum;
+
+  const toggleModal = () => setOpen(!open);
+  if (loading) return <Loader width='20px' height='20px' />;
   return (
-    <div className={cls.portfolio}>
+    <div onClick={toggleModal} className={cls.portfolio}>
       <div className={cls.title}>Your portfolio</div>
-      <span className={cls.initial}>$ {initialSum}</span> +{" "}
-      <span className={cls["portfolio__add"]}>{add}</span>{" "}
-      <span className={cls.diff}>{diff.toFixed(2)}%</span>
+      {initialSum === 0 ? (
+        <>
+          <span className={cls.initial}>$ {sum.toFixed(2)}</span>
+        </>
+      ) : (
+        <>
+          <span className={cls.initial}>$ {initialSum.toFixed(2)}</span> +{" "}
+          <span className={cls["portfolio__add"]}>{add.toFixed(4)}</span>{" "}
+          <span className={cls.diff}>{diff.toFixed(4)}%</span>
+        </>
+      )}
+      {open && <PortfolioModal toggleModal={toggleModal} />}
     </div>
   );
 }
